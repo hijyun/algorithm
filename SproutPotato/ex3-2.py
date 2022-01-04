deb81a4e75230a096d4fd2a39388eecc7faa0d65
hijyun <wilkil526@gmail.com>
2022-01-04 14:40:21 +0900

    숫자 카드 게임

--- SproutPotato/ex3-2.py
+++ SproutPotato/ex3-2.py
@@ -6,7 +6,7 @@ data = list(map(int, input().split()))
 
 data.sort() # 임력받은 수들 정렬하기
 first = data[n - 1] # 가장 큰 수
-secod = data[n - 2] # 두 번째로 큰 수
+second = data[n - 2] # 두 번째로 큰 수
 
 # 가장 큰 수가 더해지는 횟수 계산
 count = int(m / (k+1)) * k
